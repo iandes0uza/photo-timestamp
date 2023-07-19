fc4a35fdbdbab26e99d3ee29590461a16333554c
Ian <ianagdesouza2002@gmail.com>
2023-07-18 23:11:49 -0400

    Created a better format matching my old camera, tinkering with colours, sizes and placements.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # import all the libraries
-from PIL import Image
+from PIL import Image, ExifTags
 from PIL import ImageFont
 from PIL import ImageDraw
-from PIL.ExifTags import TAGS
+from PIL.ExifTags import TAGS, Base
 from tkinter import filedialog
 from tkinter import Tk
 
@@ -10,45 +10,53 @@ root = Tk()
 root.withdraw()
 
 filename = filedialog.askopenfilename(initialdir='/Users/ian/Documents/GitHub/photo-timestamp')
-print(filename)
+# print(filename)
 
+# Watermark object, stores values from parser and returns watermark string
+class wmk:
+    def __init__(self, d, m, y, hr, min, sec):
+        self.d = d
+        self.m = m
+        self.y = y
+        self.hr = hr
+        self.min = min
+        self.sec = sec
+
+    def format_date(self):
+        return (str(self.m) + "/" + str(self.d) + "/" + str(self.y))
+    def format_dateAndTime(self):
+        return (self.format_date() + " " +  str(self.hr) + ":" + str(self.min) + ":" + str(self.sec))
+
+# This function adds the watermark to the photo
 def add_watermark(img, text):
-    canvas = Image.open(img)
-    width, height = canvas.size
-    draw = ImageDraw.Draw(canvas)
-    font_size = int(width / 8)
-    font = ImageFont.truetype('arial.ttf', font_size)
-    x, y = int(width/2), int(height/2)
-    draw.text((x,y), text, font=font, fill='#FFF', stroke_width=5, stroke_fill='#222', anchor='ms')
+    width, height = img.size
+    draw = ImageDraw.Draw(img)
+    font_size = int(width / 20)
+    font = ImageFont.truetype('seven_segment.ttf', font_size)
+    x, y = int(width/3), int(height/3)
+    draw.text((0,0), text, font=font, fill='#FFFF00', stroke_width=1, stroke_fill='#222', anchor='rs')
+    img.show()
 
-    canvas.show()
+# This function grabs the timestamp from the image's EXIF data
+def get_timestamp(img_exif):
+    for (k,v) in img_exif.items():
+        if TAGS.get(k) == 'DateTimeOriginal':
+            return v
 
-def get_metadata(img):
-    canvas = Image.open(img)
-    info_dict = {
-        "Filename": canvas.filename,
-        "Image Size": canvas.size,
-        "Image Height": canvas.height,
-        "Image Width": canvas.width,
-        "Image Format": canvas.format,
-        "Image Mode": canvas.mode,
-        "Image is Animated": getattr(canvas, "is_animated", False),
-        "Frames in Image": getattr(canvas, "n_frames", 1)
-    }
-    for label,value in info_dict.items():
-        print(f"{label:25}: {value}")
-    
-    exifdata = canvas.getexif()
-    # iterating over all EXIF data fields
-    for tag_id in exifdata:
-        # get the tag name, instead of human unreadable tag id
-        tag = TAGS.get(tag_id, tag_id)
-        data = exifdata.get(tag_id)
-        # decode bytes 
-        if isinstance(data, bytes):
-            data = data.decode()
-        print(f"{tag:25}: {data}")
+# This function splits timestamp into seperate sections (d/m/y etc). This portion changes based on how EXIF data is read        
+def parse_metadata(img):
+    exif = img._getexif()
+    timestamp = str(get_timestamp(exif))
+    date, time = timestamp.split(' ')
+    y, m, d = date.split(':')
+    hr, min, sec = time.split(':')
+    return wmk(d, m, y, hr, min, sec)
 
+def main():
+    # Open image 
+    image = Image.open(filename)
+    watermark = parse_metadata(image)
+    add_watermark(image, watermark.format_dateAndTime())
 
-get_metadata(filename)
-# add_watermark(filename, 'meow')
+if __name__ == "__main__":
+    main()
